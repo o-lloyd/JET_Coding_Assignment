d78af30b9e24e49a522c4e0866f471f0d118e49d
o-lloyd <oliverhenryl@hotmail.com>
2026-03-25 17:12:11 +0000

    Get restaurant data and functional print

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
 import requests
 
-postcode = "TN173HN"
+postcode = "M204AG"
 
 url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
 
@@ -8,8 +8,21 @@ headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
-# Send a GET request to the URL
+# Send GET request to the API
 response = requests.get(url, headers=headers)
 
-# Print the HTTP status code
-print(f"Status Code: {response.status_code}")
+# If the GET request was successful
+if response.status_code == 200:
+    # Turn json response into python dictionary
+    data = response.json()
+
+    # Get data of the first ten restaurants
+    restaurants = data['restaurants'][:10]
+
+    print(f"Restaurants near {postcode}:\n")
+
+    for restaurant in restaurants:
+        print({restaurant['name']})
+        print(f"Cuisines: {restaurant['cuisines']}")
+        print(f"Rating: {restaurant['rating']['starRating']}")
+        print(f"Address: {restaurant['address']}\n")
